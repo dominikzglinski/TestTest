686cdabc05600fe51c40c68fd2725b1dfbccdffa
dominikzglinski <ddominikzglinski@gmail.com>
2021-07-03 13:24:42 +0200

    First Change

---
+++ TestTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Second Branch");
+
+
+        }
+    }
+}
